ef14ef401611b8964aa4b13d02af9bf523a5fa27
shailesh-arch <shailesh.vashishth_cs16@gla.ac.in>
2020-12-17 13:08:37 +0530

    adding git to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticlesComponent } from './articles/articles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticlesComponent
   ],
   imports: [
     BrowserModule,
